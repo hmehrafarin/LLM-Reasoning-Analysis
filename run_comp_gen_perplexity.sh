9c7e2980bf3f12092c835a63cb90c55c6db0aa1f
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2023-11-29 09:41:41 +0000

    experiment added for computing perplexity

--- run_comp_gen_perplexity.sh
+++ run_comp_gen_perplexity.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python perplexity_calculator.py --base_model 'meta-llama/Llama-2-13b-chat-hf' --prompt_template 'QASC_QA (perplexity)' &
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python compute_perplexity.py --base_model 'meta-llama/Llama-2-13b-chat-hf' --prompt_template 'QASC_QA (perplexity)' &
