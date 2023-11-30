ff2849a620756e59b857a093d434554a9ac5bf51
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2023-11-30 18:17:03 +0000

    experiment changed

--- run_comp_gen.sh
+++ run_comp_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python comp_gen.py --base_model 'meta-llama/Llama-2-13b-chat-hf' --prompt_template 'QASC-Full' --ablate_matching_words_with_answers True &
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python comp_gen.py --base_model 'meta-llama/Llama-2-13b-chat-hf' --prompt_template 'QASC-Full' --ablate_connecting_F1Q True --include_question_ablation True&
