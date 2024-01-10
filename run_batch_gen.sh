3b89cac48c59f05ff65652a394008b271487d620
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2024-01-10 17:52:49 +0000

    changes made

--- run_batch_gen.sh
+++ run_batch_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py --base_model 'meta-llama/Llama-2-7b-chat-hf' --prompt_template 'QASC-Full' --output_file_name 'generated_response-full (1 token ablated fact 2).json' --ablate_tokens_fact2 True --num_ablations 1&
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py --base_model 'google/flan-t5-xxl' --batch_size 2 --prompt_template 'QASC_Final-Answer_QA' --output_file_name 'generated_response-QA-t5.json'&
--- run_comp_gen_loglikelihood.sh
+++ run_comp_gen_loglikelihood.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python compute_loglikelihood_copy.py --base_model 'meta-llama/Llama-2-13b-chat-hf' --prompt_template 'QASC_QA (perplexity)' &
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python compute_loglikelihood_copy.py --base_model 'meta-llama/Llama-2-7b-chat-hf' --prompt_template 'QASC_Final-Answer_QA copy' &
