7f9449979e26e4581e8c08170cad41d30872cd7c
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2024-01-23 15:14:53 +0000

    changes made

--- run_batch_gen.sh
+++ run_batch_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py --base_model 'google/flan-t5-xxl' --batch_size 2 --prompt_template 'QASC_Final-Answer_QA' --output_file_name 'generated_response-QA-t5.json'&
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py \--base_model 'google/flan-t5-xxl' --batch_size 2 --prompt_template 'QASC-Full' --output_file_name 'generated_response-full-(F1F2A keyword ablation)-t5.json' --ablate_matching_words_with_answers True&
--- run_comp_gen_loglikelihood.sh
+++ run_comp_gen_loglikelihood.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python compute_loglikelihood_copy.py --base_model 'meta-llama/Llama-2-7b-chat-hf' --prompt_template 'QASC_Final-Answer_QA copy' &
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python compute_loglikelihood_copy.py --base_model 'google/flan-t5-xxl' --prompt_template 'QASC_Final-Answer_QA' &
