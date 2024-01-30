645c331a02ebc0514a8e0852b2982060f2baaf6f
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2024-01-30 16:43:48 +0000

    eval improved

--- eval.py
+++ eval.py
@@ -1,48 +1,55 @@
 import pandas as pd
 from evaluate import load
 from statistics import mean
+import fire
 
-# bertscore = load("bertscore")
+def evaluate(
+        output_file: str = "",
+        data_type: str = "QASC", # Options: QASC, Bamboogle
+        model_type: str = "llama", # Options: llama, flan-t5
+        write_output_to_file: bool = False,      
+):
+    df = pd.read_json(output_file)
+    count = 0
+    num_nulls = 0
+    tp = 0
+    fp_df = []
+    tp_df = []
 
-df = pd.read_json('generated_response-full-(F1F2A keyword ablation)-t5.json')
-count = 0
-num_nulls = 0
-tp = 0
-new_df = []
-tp_df = []
-generated_deductions = []
-actual_deductions = []
+    for i in range(len(df)):
+        count+=1
 
-for i in range(len(df)):
-    count+=1
-    # generated_deductions.append(df.iloc[i]['generated deduced'])
-    # actual_deductions.append(df.iloc[i]['actual deduced'])
-    # Accuracy
-    df.iloc[i]['pred answer'] = df.iloc[i]['pred answer'].rstrip()
-    if df.iloc[i]['pred answer'] == "O":
-        num_nulls += 1
-    # if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[3:] == df.iloc[i]['pred answer'].lower()[3:]:
-    #     tp += 1
-    #     tp_df.append(df.iloc[i])
-    if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[:3] == df.iloc[i]['pred answer'].lower()[:3]:
-        tp += 1
-        tp_df.append(df.iloc[i])        
-    else:
-        new_df.append(df.iloc[i])
+        df.iloc[i]['pred answer'] = df.iloc[i]['pred answer'].rstrip()
+        if df.iloc[i]['pred answer'] == "O":
+            num_nulls += 1
+        if data_type == "Bamboogle":
+            if df.iloc[i]['pred answer'].lower() == df.iloc[i]['true answer'].lower():
+                tp += 1
+                tp_df.append(df.iloc[i])
+        elif model_type == "llama" and data_type == "QASC":
+            if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[3:] == df.iloc[i]['pred answer'].lower()[3:]:
+              tp += 1
+              tp_df.append(df.iloc[i])
+        elif model_type == "flan-t5" and data_type == "QASC":
+            if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[:3] == df.iloc[i]['pred answer'].lower()[:3]:
+                tp += 1
+                tp_df.append(df.iloc[i])                 
+        else:
+            fp_df.append(df.iloc[i])
 
-# results = bertscore.compute(predictions=generated_deductions, references=actual_deductions, lang="en")
 
-new_df = pd.DataFrame(new_df)
+    fp_df = pd.DataFrame(fp_df)
 
-tp_df = pd.DataFrame(tp_df)
-# unmatched_df = pd.DataFrame(unmatched)
-# unmatched_df.to_json("unmatched_QA.json", orient ='records')
-# tp_df.to_json("True_positives_full.json", orient ='records')
-# new_df.to_json("False_positives_full.json", orient ='records')
-print("Number of Instances: {}".format(count))
-print("Number of Nulls: {}".format(num_nulls))
-print("True Positive: {}".format(tp))
-print("Accuracy: {}".format(tp/count))
-# print(results['f1'].count(1.0))
-# print(mean(results['f1']))
+    tp_df = pd.DataFrame(tp_df)
+    if write_output_to_file:
+        tp_df.to_json("True_positives_full.json", orient ='records')
+        fp_df.to_json("False_positives_full.json", orient ='records')
+
+    print("Number of Instances: {}".format(count))
+    print("Number of Nulls: {}".format(num_nulls))
+    print("True Positive: {}".format(tp))
+    print("Accuracy: {}".format(tp/count))
+
+if __name__ == "__main__":
+    fire.Fire(evaluate)
 
