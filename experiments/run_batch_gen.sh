4ed3afd31af58296b1e6c83cc310fc9088b7b253
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2024-01-30 16:55:32 +0000

    experiment folder created

--- experiments/run_batch_gen.sh
+++ experiments/run_batch_gen.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py \--base_model 'google/flan-t5-xxl' --batch_size 2 --prompt_template 'QASC-Full' --output_file_name 'generated_response-full-(F1F2A keyword ablation)-t5.json' --ablate_matching_words_with_answers True&
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py \
+ --base_model 'google/flan-t5-xxl' \
+ --batch_size 2 \
+ --prompt_template 'Bamboogle-Full' \
+ --output_file_name 'generated_response_Bamboogle_Full_flan-t5 (shuffled both facts).json' \
+ --shuffle_fact1 True \
+ --shuffle_fact2 True \
+ --data_path 'data/bamboogle-with-facts.json'&
---
+++ experiments/run_eval.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python eval.py \
+ --output_file 'generated_response_Bamboogle_QA_flan-t5.json' \
+ --data_type 'Bamboogle' \
+ --model_type 'flan-t5'
