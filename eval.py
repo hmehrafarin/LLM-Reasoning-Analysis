519295ea0958cc96eb8a2af6ff26766c3977db2a
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2024-02-06 15:04:01 +0000

    rouge metric added

--- eval.py
+++ eval.py
@@ -1,13 +1,14 @@
 import pandas as pd
-from evaluate import load
+import evaluate
 from statistics import mean
 import fire
 
-def evaluate(
+def eval(
         output_file: str = "",
         data_type: str = "QASC", # Options: QASC, Bamboogle
         model_type: str = "llama", # Options: llama, flan-t5
-        write_output_to_file: bool = False,      
+        write_output_to_file: bool = False,     
+        metric: str = "accuracy" # Options: accuracy, rouge 
 ):
     df = pd.read_json(output_file)
     count = 0
@@ -15,41 +16,46 @@ def evaluate(
     tp = 0
     fp_df = []
     tp_df = []
+    if metric == "accuracy":
+        for i in range(len(df)):
+            count+=1
+            df.iloc[i]['pred answer'] = df.iloc[i]['pred answer'].rstrip()
+            if df.iloc[i]['pred answer'] == "O":
+                num_nulls += 1
+            if data_type == "Bamboogle":
+                if df.iloc[i]['pred answer'].lower() == df.iloc[i]['true answer'].lower():
+                    tp += 1
+                    tp_df.append(df.iloc[i])
+            elif model_type == "llama" and data_type == "QASC":
+                if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[3:] == df.iloc[i]['pred answer'].lower()[3:]:
+                  tp += 1
+                  tp_df.append(df.iloc[i])
+            elif model_type == "flan-t5" and data_type == "QASC":
+                if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[:3] == df.iloc[i]['pred answer'].lower()[:3]:
+                    tp += 1
+                    tp_df.append(df.iloc[i])                 
+            else:
+                fp_df.append(df.iloc[i]) 
 
-    for i in range(len(df)):
-        count+=1
+        fp_df = pd.DataFrame(fp_df)
+        tp_df = pd.DataFrame(tp_df)
+        if write_output_to_file:
+            tp_df.to_json("True_positives_full.json", orient ='records')
+            fp_df.to_json("False_positives_full.json", orient ='records')
 
-        df.iloc[i]['pred answer'] = df.iloc[i]['pred answer'].rstrip()
-        if df.iloc[i]['pred answer'] == "O":
-            num_nulls += 1
-        if data_type == "Bamboogle":
-            if df.iloc[i]['pred answer'].lower() == df.iloc[i]['true answer'].lower():
-                tp += 1
-                tp_df.append(df.iloc[i])
-        elif model_type == "llama" and data_type == "QASC":
-            if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[3:] == df.iloc[i]['pred answer'].lower()[3:]:
-              tp += 1
-              tp_df.append(df.iloc[i])
-        elif model_type == "flan-t5" and data_type == "QASC":
-            if df.iloc[i]['true answer'].lower() == df.iloc[i]['pred answer'].lower() or df.iloc[i]['true answer'].lower()[:3] == df.iloc[i]['pred answer'].lower()[:3]:
-                tp += 1
-                tp_df.append(df.iloc[i])                 
-        else:
-            fp_df.append(df.iloc[i])
+        print("Number of Instances: {}".format(count))
+        print("Number of Nulls: {}".format(num_nulls))
+        print("True Positive: {}".format(tp))
+        print("Accuracy: {}".format(tp/count))
 
+    elif metric == "rouge":
+        rouge = evaluate.load('rouge')
+        predictions = df['pred answer']
+        gold = df['true answer']
+        rouge_scores = rouge.compute(predictions=predictions, references=gold)
+        print(rouge_scores)
 
-    fp_df = pd.DataFrame(fp_df)
-
-    tp_df = pd.DataFrame(tp_df)
-    if write_output_to_file:
-        tp_df.to_json("True_positives_full.json", orient ='records')
-        fp_df.to_json("False_positives_full.json", orient ='records')
-
-    print("Number of Instances: {}".format(count))
-    print("Number of Nulls: {}".format(num_nulls))
-    print("True Positive: {}".format(tp))
-    print("Accuracy: {}".format(tp/count))
 
 if __name__ == "__main__":
-    fire.Fire(evaluate)
+    fire.Fire(eval)
 
