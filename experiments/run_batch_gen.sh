e14d9c95a3ff9dcc076f3746e52908471a7be11e
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2024-02-06 23:24:58 +0000

    responses added

--- experiments/run_batch_gen.sh
+++ experiments/run_batch_gen.sh
@@ -3,9 +3,9 @@
 srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py \
  --base_model 'google/flan-t5-xxl' \
  --batch_size 2 \
- --prompt_template 'QASC-Full' \
- --output_file_name 'QASC-full (F1Q connecting ablation).json' \
- --data_path 'data/eval_data.json' \
- --ablate_connecting_F1Q True&
+ --prompt_template 'QASC-QAF (fact 2 only)' \
+ --output_file_name 'QASC-QAF (fact 2 only) \                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   )).json' \
+ --data_path 'data/eval_data.json'&
+#  --ablate_connecting_F1F2 True&
 #  --shuffle_fact1 True \
 #  --shuffle_fact2 True&
--- experiments/run_eval.sh
+++ experiments/run_eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python eval.py \
-    --output_file 'QASC-flan-T5-xxl/QASC-QAF.json' \
+    --output_file 'QASC-full (F1F2 connecting ablation).json' \
     --data_type 'QASC' \
     --model_type 'flan-t5' \
     --metric 'accuracy'
