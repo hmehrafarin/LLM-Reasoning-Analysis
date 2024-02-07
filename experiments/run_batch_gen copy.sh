6c0b8484a2b3bc3003fcb673c2345c57fe630829
Houman Mehrafarin <houman.mehrafarin@gmail.com>
2024-02-07 10:49:55 +0000

    changes made

---
+++ experiments/run_batch_gen copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py \
+    --base_model 'google/flan-t5-xxl' \
+    --batch_size 2 \
+    --prompt_template 'QASC-QAF (fact 2 only)' \
+    --output_file_name 'QASC-QAF (fact 2 only).json' \
+    --data_path 'data/eval_data.json'&
