6fbf5e2355f0bc354d72fff57a4f34de63b215f5
hmehrafarin <houman.mehrafarin@gmail.com>
2024-09-27 17:00:03 +0100

    changes made

--- experiments/run_batch_generate.sh
+++ experiments/run_batch_generate.sh
@@ -3,9 +3,9 @@
 srun -u --nodes=1 --mem=200G --time=10:00:00 --partition=gpu --gres=gpu:1 python batch_generate.py \
     --base_model 'google/flan-t5-xxl' \
     --batch_size 2 \
-    --prompt_template 'Bamboogle-Jibberish-Full' \
+    --prompt_template 'Bamboogle-gibberish-Full' \
     --output_file_name 'Bamboogle-flan-T5-output/Bamboogle-full-gibberish (both facts shuffled)-seed:123.json' \
-    --data_path 'data/bamboogle-jibberish.json' \
+    --data_path 'data/bamboogle-gibberish.json' \
     --random_seed 123 \
     --shuffle_fact1 True \
     --shuffle_fact2 True&
